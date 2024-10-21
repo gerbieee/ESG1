0870d18b3cc71a0c763f69d1ccc32278f0c8f129
gerbiebansagan <gerbiebansagan@gmail.com>
2024-10-21 18:32:03 +0800

    Design Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
-    <div className="flex flex-col w-[100px]">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-black font-sans flex flex-col gap-4 p-4  shadow-md rounded bg-gray-200">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(p) => setPassword(p.target.value)}
+        />
 
+        <p>{email === 'james' ? email : ''}</p>
+        <p>{password === 'james' ? password : ""}</p>
 
-
-
-
-  <label htmlFor="fname">Email:</label>
-  <input type="text" id="fname" name="fname" value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
-
-  
-  <label htmlFor="fname">Password:</label>
-  <input type="password" id="fname" name="fname" value={password} onChange={(p) => {setPassword(p.target.value)}}></input>
-
-
-
-
-
-
-  <input type="submit" value="Submit"></input>
-<p>{email === "james" ? email : "wrong"}</p>
-<p>{password === "james" ? password : "lagyan mo ng password bobo"}</p>
-
-
+        <input type="submit" value="Submit" className="bg-blue-900 text-white py-2 rounded" / >
+      </div>
     </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
