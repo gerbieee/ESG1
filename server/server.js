46273ba9dc839088f7f253180a77c27c3a6289c9
ficuno <eldritchgallows@gmail.com>
2024-11-06 01:30:15 +0800

    test: update code

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const server = express();
 
 server.use(express.json());
 server.use(
-	cors()
+	cors({
+		origin: process.env.ENV !== "production" ? "http://localhost:5173" : "https://cvsu-bacoor.vercel.app",
+		methods: ["GET", "POST"],
+		credentials: true,
+	})
 );
 
 const db = mysql.createPool({
@@ -21,6 +25,91 @@ const db = mysql.createPool({
 	port: process.env.DB_PORT,
 });
 
-server.get("/", () => {
-	console.log("good mourning.");
+server.get("/", (_, res) => {
+    res.json("good mourning.");
+});
+
+server.post("/api/register-student-account", async (req, res) => {
+	// recieve info from user
+	const { email, password } = req.body;
+
+	try {
+		// check if email is already used
+		const [getAccount] = await db.query(
+			`
+			SELECT email
+			FROM accounts
+			WHERE email = ?
+			`,
+			[email]
+		);
+
+		// respond with error
+		if (getAccount.length > 0) {
+			return res.status(404).json({
+				message: "Email address is already used.",
+				isEmailAlreadyUsed: true,
+			});
+		}
+
+		// encrypt password
+		const hashedPassword = await bcrypt.hash(password, 8);
+
+		// insert new account to db
+		await db.query(
+			`
+			INSERT INTO accounts (email, password, user_type)
+			VALUES (?, ?, ?)
+			`,
+			[email, hashedPassword, "newStudent"] // values
+		);
+
+		// respond with success
+		return res.status(200).json({ message: "Account registered." });
+	} catch (err) {
+		// respond with error
+		console.error(err);
+		return res.status(500).json({ error: "An error occurred while querying the database." });
+	}
+});
+
+server.post("/api/sign-in", async (req, res) => {
+	// recieve info from user
+	const { email, password } = req.body;
+
+	try {
+		// check if email matches in db
+		const [getAccount] = await db.query(
+			`
+			SELECT email, password
+			FROM accounts
+			WHERE email = ?
+			`,
+			[email]
+		);
+
+		// respond with error
+		if (getAccount.length === 0) {
+			return res.status(404).json({ message: "Invalid sign in credentials." });
+		}
+
+		// check if passwords match
+		const isPasswordMatched = await bcrypt.compare(password, getAccount[0].password);
+
+		// respond with error
+		if (!isPasswordMatched) {
+			return res.status(404).json({ message: "Invalid sign in credentials." });
+		}
+
+		// respond with sucess
+		return res.status(200).json({ message: "Account signed in." });
+	} catch (err) {
+		// respond with error
+		console.error(err);
+		return res.status(500).json({ error: "An error occurred while querying the database." });
+	}
+});
+
+server.listen(8080, () => {
+	console.log("Connected to the server.");
 });
