b956da0db7161b1d0f7c9fa0829a9d4de501fec0
ficuno <eldritchgallows@gmail.com>
2024-11-05 23:37:13 +0800

    test: add env var logging

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const ENV = process.env.VITE_ENV || "development";
 const BACKEND_URL = process.env.VITE_BACKEND_URL || "http://localhost:8080";
+console.log(process.env.VITE_ENV);
+console.log(process.env.VITE_BACKEND_URL);
 
 // https://vitejs.dev/config/
 export default defineConfig({
