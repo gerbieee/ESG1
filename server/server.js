e7072938f8d9c97214c18426f420b8bfeb726650
ficuno <eldritchgallows@gmail.com>
2024-11-06 00:40:09 +0800

    fix: resolve cors issue

--- server/server.js
+++ server/server.js
@@ -11,10 +11,7 @@ const server = express();
 server.use(express.json());
 server.use(
 	cors({
-		origin:
-			ENV !== "production"
-				? "http://localhost:5173"
-				: ["https://esg1-client-1my6jcy5w-cheiron.vercel.app", "https://cvsu-bacoor.vercel.app"],
+		origin: ENV !== "production" ? "http://localhost:5173" : "https://cvsu-bacoor.vercel.app",
 		methods: ["GET", "POST"],
 		credentials: true,
 	})
